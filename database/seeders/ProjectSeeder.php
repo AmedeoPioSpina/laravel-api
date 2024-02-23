499a4fd9b5b26f5dcf44ae38a2aab9141d597195
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-02-23 17:30:27 +0100

    il json arriva nella rotta indicata

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        //
+        for($i=0; $i<10; $i++){
+            $newProject = new Project();
+            $newProject->name = $faker->word();
+            $newProject->save();
+        }
+    }
+}
